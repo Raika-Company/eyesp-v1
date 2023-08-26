7a5cc838ae0d568dcdace4e59882fe31dfb39cb3
amirkhoda8 <amir.hosein.clan@gmail.com>
2023-08-26 16:10:24 +0330

    add history

---
+++ client-app/src/components/testHistory/DlandUlCharts.jsx
@@ -0,0 +1,20 @@
+import { Box } from "@mui/material";
+import React from "react";
+
+function DlandUlCharts() {
+  return (
+    <>
+      <Box
+      // sx={{
+      //   border: "2px solid gray",
+      //   width: "50%",
+      //   height: "20em",
+      // }}
+      >
+        fgrtgtht
+      </Box>
+    </>
+  );
+}
+
+export default DlandUlCharts;
