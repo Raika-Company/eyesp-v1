8a2821dae1d3d84f87965380d3f53b81ef04ea70
amirkhoda8 <amir.hosein.clan@gmail.com>
2023-08-26 17:18:58 +0330

    add boxes in testHistory

--- client-app/src/components/testHistory/DlandUlCharts.jsx
+++ client-app/src/components/testHistory/DlandUlCharts.jsx
@@ -1,18 +1,19 @@
+import styled from "@emotion/styled";
 import { Box } from "@mui/material";
 import React from "react";
+
+const CustomBox = styled(Box)(() => ({
+  width: "60em",
+  height: "20em",
+  border: "2px solid #E0E0E0",
+  borderRadius: "2em",
+  margin: " 0 auto",
+}));
 
 function DlandUlCharts() {
   return (
     <>
-      <Box
-      // sx={{
-      //   border: "2px solid gray",
-      //   width: "50%",
-      //   height: "20em",
-      // }}
-      >
-        fgrtgtht
-      </Box>
+      <CustomBox></CustomBox>
     </>
   );
 }
