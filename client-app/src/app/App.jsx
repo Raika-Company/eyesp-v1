95f12ccd717a898aa441ce5385552d92c4fa7e29
Sina Sepahvand <sina_sepahvand@email.kntu.ac.ir>
2023-08-27 06:01:42 +0400

    Add theme

--- client-app/src/app/App.jsx
+++ client-app/src/app/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
-
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { lightTheme } from './Palette';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-    </>
+    <ThemeProvider theme={lightTheme}>
+    </ThemeProvider>
   )
 }
 
