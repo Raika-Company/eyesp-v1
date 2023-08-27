1080ff0cfba534b4ec28625dd56c41ddc34e7cd6
Sina Sepahvand <sina_sepahvand@email.kntu.ac.ir>
2023-08-27 07:07:14 +0400

    add navbar

--- client-app/src/app/App.jsx
+++ client-app/src/app/App.jsx
@@ -1,14 +1,22 @@
-import { useState } from 'react'
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { lightTheme } from './Palette';
-import './App.css'
+import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Navbar from "../components/navbar/Navbar";
+import { lightTheme } from "./Palette";
+import "./App.css";
+import SpeedTest from "../components/speedtest/SpeedTest";
 
 function App() {
-
   return (
     <ThemeProvider theme={lightTheme}>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/speedtest" element={<SpeedTest />} />
+        </Routes>
+      </Router>
     </ThemeProvider>
-  )
+  );
 }
 
-export default App
+export default App;
