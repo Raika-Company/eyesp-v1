5ed8c4973096ddeba9c160bc5872c0db09f15762
amirkhoda8 <74904393+amirkhoda@users.noreply.github.com>
2023-08-28 15:44:02 +0330

    handle the buge of the dark and light theme

--- client-app/src/app/Palette.js
+++ client-app/src/app/Palette.js
@@ -1,123 +1,122 @@
-import { createTheme } from '@mui/material';
+import { createTheme } from "@mui/material";
 
 export const lightTheme = createTheme({
-    direction: "rtl",
-    palette: {
-        mode: "light",
-        primary: {
-            main: "#126AED"
-        },
-        secondary: {
-            main: "#DB7F12"
-        },
-        background: {
-            default: "linear-gradient(195deg, #FCFCFF 24.09%, #EBEBEB 100%)",
-        },
+  direction: "rtl",
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#126AED",
     },
-    typography: {
-        h1: {
-            fontFamily: "PeidaBold, serif",
-        },
-        h2: {
-            fontFamily: "PeidaBold, serif",
-        },
-        h3: {
-            fontFamily: "PeydaSemiBold, serif",
-            fontSize: "36px"
-        },
-        h4: {
-            fontFamily: "PeydaSemiBold, serif",
-            textEdge: "cap",
-            leadingTrim: "both",
-            fontSize: 30,
-            fontWeight: 600,
-            lineHeight: "normal",
-            fontStyle: "normal",
-
-        },
-        h5: {
-            fontFamily: "PeydaMedium, serif",
-            color: "#A4A4A4",
-            fontWeight: 700,
-            fontSize: 25,
-            lineHeight: "normal",
-            fontStyle: "normal",
-        },
-        h6: {
-            fontFamily: "PeydaMedium, serif",
-            fontSize: 25
-        },
-        body1: {
-            fontFamily: "PeydaMedium, sans-serif",
-            fontSize: 18
-        },
-        body2: {
-            fontFamily: "PeydaMedium, sans-serif",
-            fontSize: 16
-        },
-        subtitle1: {
-            fontFamily: "PeydaMedium, sans-serif",
-            fontSize: 16,
-            opacity: 0.3,
-            color: "#000"
-        },
-        fontFamily: "PeydaMedium"
+    secondary: {
+      main: "#DB7F12",
     },
-    components: {
-        MuiInputLabel: {
-            styleOverrides: {
-                root: {
-                    left: "inherit",
-                    right: "1rem",
-                    transformOrigin: "right",
-                },
-            },
-        },
-        MuiOutlinedInput: {
-            styleOverrides: {
-                notchedOutline: {
-                    textAlign: "right",
-                },
-            },
-        },
-        MuiDrawer: {
-            styleOverrides: {
-                root: {
-                    direction: "ltr",
-                },
-                paper: {
-                    position: 'absolute',
-                    left: 0,
-                    transition: 'transform 225ms cubic-bezier(0, 0, 0.2, 1) 0ms',
-                    '&.MuiDrawer-open': {
-                        transform: 'translateX(0)'
-                    },
-                    '&:not(.MuiDrawer-open)': {
-                        transform: 'translateX(-100%)'
-                    }
-                }
-            },
-        }
+    background: {
+      default: "linear-gradient(195deg, #FCFCFF 24.09%, #EBEBEB 100%)",
     },
+  },
+  typography: {
+    h1: {
+      fontFamily: "PeidaBold, serif",
+    },
+    h2: {
+      fontFamily: "PeidaBold, serif",
+    },
+    h3: {
+      fontFamily: "PeydaSemiBold, serif",
+      fontSize: "36px",
+    },
+    h4: {
+      fontFamily: "PeydaSemiBold, serif",
+      textEdge: "cap",
+      leadingTrim: "both",
+      fontSize: 30,
+      fontWeight: 600,
+      lineHeight: "normal",
+      fontStyle: "normal",
+    },
+    h5: {
+      fontFamily: "PeydaMedium, serif",
+      color: "#A4A4A4",
+      fontWeight: 700,
+      fontSize: 25,
+      lineHeight: "normal",
+      fontStyle: "normal",
+    },
+    h6: {
+      fontFamily: "PeydaMedium, serif",
+      fontSize: 25,
+    },
+    body1: {
+      fontFamily: "PeydaMedium, sans-serif",
+      fontSize: 18,
+    },
+    body2: {
+      fontFamily: "PeydaMedium, sans-serif",
+      fontSize: 16,
+    },
+    subtitle1: {
+      fontFamily: "PeydaMedium, sans-serif",
+      fontSize: 16,
+      opacity: 0.3,
+      color: "#000",
+    },
+    fontFamily: "PeydaMedium",
+  },
+  components: {
+    MuiInputLabel: {
+      styleOverrides: {
+        root: {
+          left: "inherit",
+          right: "1rem",
+          transformOrigin: "right",
+        },
+      },
+    },
+    MuiOutlinedInput: {
+      styleOverrides: {
+        notchedOutline: {
+          textAlign: "right",
+        },
+      },
+    },
+    MuiDrawer: {
+      styleOverrides: {
+        root: {
+          direction: "ltr",
+        },
+        paper: {
+          position: "absolute",
+          left: 0,
+          transition: "transform 225ms cubic-bezier(0, 0, 0.2, 1) 0ms",
+          "&.MuiDrawer-open": {
+            transform: "translateX(0)",
+          },
+          "&:not(.MuiDrawer-open)": {
+            transform: "translateX(-100%)",
+          },
+        },
+      },
+    },
+  },
 });
 
 export const darkTheme = createTheme({
-    ...lightTheme,
-    palette: {
-        ...lightTheme.palette,
-        mode: "dark",
-        primary: {
-            main: "#315A9E"
-        },
-        secondary: {
-            main: "#B0680F"
-        },
-        background: {
-            default: "linear-gradient(195deg, #222 24.09%, #111 100%)",
-        },
-        text: {
-            primary: "#FFF",
-            secondary: "#BBB"
-        },
-    }
+  ...lightTheme,
+  palette: {
+    ...lightTheme.palette,
+    mode: "dark",
+    primary: {
+      main: "#315A9E",
+    },
+    secondary: {
+      main: "#B0680F",
+    },
+    background: {
+      default: "#000", // Set the background color to black
+    },
+    text: {
+      primary: "#FFF",
+      secondary: "#BBB",
+    },
+  },
 });
