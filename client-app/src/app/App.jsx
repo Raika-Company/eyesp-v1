87c5891b14e8c405f32d5dce2ea6af9b84bc642c
amirkhoda8 <74904393+amirkhoda@users.noreply.github.com>
2023-08-28 15:44:02 +0330

    handle the buge of the dark and light theme

--- client-app/src/app/App.jsx
+++ client-app/src/app/App.jsx
@@ -1,46 +1,26 @@
-/**
- * App Component
- *
- * This is the root component for the application, setting up routing and theme.
- *
- * @module App
- * @file App.jsx Responsible for rendering the App component.
- */
-
 import { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { CssBaseline } from "@mui/material";
+import { CssBaseline, Button } from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-
-// Components
 import Navbar from "../components/navbar/Navbar";
+import { lightTheme, darkTheme } from "./Palette";
+import ThemeSwitcher from "./common/ThemeSwitcher";
 import SpeedTest from "../components/speedtest/SpeedTest";
 import TestHistory from "../components/testHistory/TestHistory";
 
-// Styles and Themes
-import { lightTheme, darkTheme } from "./Palette";
 import "./App.css";
 
-/**
- * The main App component.
- *
- * @function App
- * @returns {ReactElement} Rendered App component
- */
 function App() {
-  // State to manage current theme, light by default
   const [theme, setTheme] = useState(lightTheme);
 
-  /**
-   * Toggles between light and dark themes.
-   *
-   * @function toggleTheme
-   */
   const toggleTheme = () => {
-    setTheme(prevTheme => (prevTheme === lightTheme ? darkTheme : lightTheme));
-  }
+    if (theme === lightTheme) {
+      setTheme(darkTheme);
+    } else {
+      setTheme(lightTheme);
+    }
+  };
 
-  // Compute current theme mode
   const currentThemeMode = theme === lightTheme ? "light" : "dark";
 
   return (
