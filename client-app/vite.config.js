b282a82dd29fd1cefb104c4d71b1a716be491b5c
Sina Sepahvand <sina_sepahvand@email.kntu.ac.ir>
2023-08-29 17:15:58 +0400

    Optimize PWA app

--- client-app/vite.config.js
+++ client-app/vite.config.js
@@ -1,33 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    host: '0.0.0.0'
+    host: '0.0.0.0',
   },
   plugins: [
     react(),
     VitePWA({
-      manifest: {
-        "name": "Rayka SpeedTest",
-        "short_name": "Rayka SpeedTest",
-        "display": "standalone",
-        "start_url": "/pc",
-        "icons": [
+      registerType: 'autoUpdate',
+      manifest: '/manifest.webmanifest',
+      workbox: {
+        globPatterns: ['**/*'],
+        globIgnores: ['**/*.html'],
+        navigateFallback: '/index.html',
+        skipWaiting: true,
+        clientsClaim: true,
+        runtimeCaching: [
           {
-            "src": "android-chrome-192x192.png",
-            "sizes": "192x192",
-            "type": "image/png"
+            urlPattern: /^https?.*/,
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'all',
+              expiration: {
+                maxEntries: 500,
+                maxAgeSeconds: 60 * 60 * 24 * 7, // 1 week
+              },
+            },
           },
-          {
-            "src": "android-chrome-512x512.png",
-            "sizes": "512x512",
-            "type": "image/png"
-          }
-        ]
+        ],
       },
-    })
-  ]
-})
+    }),
+  ],
+});
