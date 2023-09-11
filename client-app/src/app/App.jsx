b87c9491a599e5cfa3229b6bf5474e3a9e82abe5
Sina Sepahvand <sina_sepahvand@email.kntu.ac.ir>
2023-09-11 15:54:08 +0400

    Add new logo

--- client-app/src/app/App.jsx
+++ client-app/src/app/App.jsx
@@ -1,7 +1,7 @@
-import { useState, lazy, Suspense } from "react";
+import { useState, Suspense } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { CssBaseline } from "@mui/material";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { ThemeProvider } from "@mui/material/styles";
 
 import Navbar from "../components/navbar/Navbar";
 import DashboardNavbar from "../components/navbar/DashboardNavbar";
