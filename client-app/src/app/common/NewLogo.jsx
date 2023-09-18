3ae502dff3ce8e653c175b1647cb5f1997194f9e
Sina Sepahvand <sina_sepahvand@email.kntu.ac.ir>
2023-09-18 16:18:37 +0400

    refactor: fix new logo

--- client-app/src/app/common/NewLogo.jsx
+++ client-app/src/app/common/NewLogo.jsx
@@ -1,30 +1,14 @@
 import React from "react";
 import { Box, Typography } from "@mui/material";
-import logo from "../../app/assets/image/logo.svg";
+import logo from "../../app/assets/image/newLogo.svg";
 
 const NewLogo = () => {
   return (
-    <>
-      {" "}
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "space-evenly",
-          backgroundColor: "#ffff",
-          alignItems: "center",
-          float: "left",
-          borderRadius: "2rem",
-          marginBottom: "1em",
-          marginTop: "2.5em",
-          width: "13%",
-          height: "6%",
-        }}
-      >
-        <Typography>EYESP.LIVE</Typography>
+    <Box display="flex" width="12.5625rem" justifyContent="space-evenly" paddingY="0.75rem" borderRadius="2rem" marginTop="2.5rem" marginRight="auto" sx={{background: "radial-gradient(232.71% 140.09% at 3.96% 11.02%, rgba(255, 255, 255, 0.71) 0%, rgba(255, 255, 255, 0.80) 43.38%, rgba(255, 255, 255, 0.51) 100%)"}}>
+      <Typography fontFamily="TrenchThin" fontSize="1.5rem">EYESP.LIVE</Typography>
 
-        <img style={{ width: "17%", height: "80%" }} src={logo} alt="logo" />
-      </Box>{" "}
-    </>
+      <img style={{ width: "32px", height: "32px" }} src={logo} alt="logo" />
+    </Box>
   );
 };
 
