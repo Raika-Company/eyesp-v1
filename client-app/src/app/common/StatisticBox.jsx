0e704010952aa83848b765372918adef3fdeb2ed
Sina Sepahvand <sina_sepahvand@email.kntu.ac.ir>
2023-09-19 11:32:11 +0400

    refactor: refactor new navbar

---
+++ client-app/src/app/common/StatisticBox.jsx
@@ -0,0 +1,41 @@
+import { Box, Typography } from "@mui/material";
+
+const StatisticBox = ({ background, title, unit, value }) => (
+  <Box
+    sx={{
+      background,
+      height: "8.1875rem",
+      borderRadius: "1.25rem",
+      position: "relative",
+      padding: "1rem",
+    }}
+  >
+    <Typography
+      color="#676767"
+      fontSize="1.25rem"
+      fontFamily="PeydaRegular"
+      sx={{ alignSelf: "flex-end" }}
+    >
+      {title}:
+    </Typography>
+    <Typography
+      color="#676767"
+      fontSize="2.5rem"
+      fontFamily="PeydaLight"
+      fontWeight="300"
+      sx={{ position: "absolute", bottom: "1rem", left: "1rem" }}
+    >
+      <Typography
+        color="#676767"
+        fontFamily="PeydaRegular"
+        fontSize="1.25rem"
+        component="span"
+      >
+        {unit}
+      </Typography>
+      {value}
+    </Typography>
+  </Box>
+);
+
+export default StatisticBox;
