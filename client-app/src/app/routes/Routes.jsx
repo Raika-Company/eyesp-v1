542d04f0f5e2d08349f1320fe2aaf2cf5331a8bf
Sina Sepahvand <sina_sepahvand@email.kntu.ac.ir>
2023-09-25 10:58:49 +0400

    feat: add functionality to sped test

--- client-app/src/app/routes/Routes.jsx
+++ client-app/src/app/routes/Routes.jsx
@@ -1,107 +1,87 @@
 import React, { lazy } from "react";
 
-/**
- * Utility function to lazy load components based on the given path.
- * @param {string} path - The path to the component.
- * @returns {React.LazyExoticComponent} - A lazy-loaded component.
- */
+const SpeedTest = lazy(() =>
+  import("../../pages/Speedtest/SpeedTest")
+);
 
-// Utility function to lazy load components
-const lazyLoad = (path) => lazy(() => import(`../../pages/${path}`));
+const Dashboard = lazy(() =>
+  import("../../pages/dashboard/Dashboard")
+);
+const Information = lazy(() =>
+  import("../../pages/information/Information")
+);
+const OperatorPerformance = lazy(() =>
+  import("../../pages/ispPerformance/OperatorPerformance")
+);
 
-const SpeedTest = lazyLoad("speedtest/SpeedTest");
-const Dashboard = lazyLoad("dashboard/Dashboard");
-const Information = lazyLoad("information/Information");
-const OperatorPerformance = lazyLoad("ispPerformance/OperatorPerformance");
-const TestHistory = lazyLoad("testHistory/TestHistory");
-const MyISP = lazyLoad("myISP/MyISP");
-const Province = lazyLoad("dashboard/province/Province");
+const TestHistory = lazy(() =>
+  import("../../pages/testHistory/TestHistory")
+);
 
-/**
- * Creates a route object for use within the routing system.
- * @param {string} path - The URL path for the route.
- * @param {React.ComponentType} component - The component to render at this path.
- * @param {string} title - The title for the route.
- * @param {string} description - The description for the route.
- * @param {string} keywords - The SEO keywords for the route.
- * @param {string} robots - The robots meta tag content for the route.
- * @returns {Object} - A route object.
- */
-const createRoute = (
-  path,
-  component,
-  title,
-  description,
-  keywords,
-  robots
-) => ({
-  path,
-  element: React.createElement(component),
-  title: `${title} | TIC Radar`,
-  description,
-  keywords,
-  robots,
-});
+const MyISP = lazy(() => import("../../pages/myISP/MyISP"));
 
-/**
- * An array of main routes for the application.
- * @type {Array<Object>}
- */
+const Province = lazy(() =>
+  import("../../pages/dashboard/province/Province")
+);
+
 export const mainRoutes = [
-  createRoute(
-    "/",
-    SpeedTest,
-    "Speed Test",
-    "Test your internet speed with our reliable and fast speed testing tool.",
-    "speed test, internet speed, broadband speed, download speed, upload speed",
-    "index, follow"
-  ),
-  createRoute(
-    "/dashboard",
-    Dashboard,
-    "Dashboard",
-    "Admin dashboard to manage and review system metrics.",
-    "dashboard, admin panel, system metrics",
-    "noindex, nofollow"
-  ),
-  createRoute(
-    "/information",
-    Information,
-    "Information",
-    "Defining concepts related to the Internet and its disorders.",
-    "information, internet, internet speed, broadband speed, download speed, upload speed",
-    "index, follow"
-  ),
-  createRoute(
-    "/history",
-    TestHistory,
-    "Test History",
-    "Review all your past internet speed tests in one place.",
-    "test history, speed results, past tests",
-    "index, follow"
-  ),
-  createRoute(
-    "/my-isp",
-    MyISP,
-    "My ISP",
-    "Review all your past internet speed tests in one place.",
-    "test history, speed results, past tests",
-    "index, follow"
-  ),
-  createRoute(
-    "/operator-performance",
-    OperatorPerformance,
-    "Operator Performance",
-    "Review all your past internet speed tests in one place.",
-    "test history, speed results, past tests",
-    "index, follow"
-  ),
-  createRoute(
-    "/dashboard/:provinceName",
-    Province,
-    "Province Details",
-    "Detailed insights into specific province metrics and data.",
-    "province, details, insights, metrics",
-    "noindex, nofollow"
-  ),
+  {
+    path: "/",
+    element: <SpeedTest />,
+    title: "Speed Test | TIC Radar",
+    description:
+      "Test your internet speed with our reliable and fast speed testing tool.",
+    keywords:
+      "speed test, internet speed, broadband speed, download speed, upload speed",
+    robots: "index, follow",
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
+    title: "Dashboard | TIC Radar",
+    description: "Admin dashboard to manage and review system metrics.",
+    keywords: "dashboard, admin panel, system metrics",
+    robots: "noindex, nofollow", // Preventing search engines from indexing admin pages.
+  },
+  {
+    path: "/information",
+    element: <Information />,
+    description: "Defining concepts related to the Internet and its disorders.",
+    title: "Information | TIC Radar",
+    keywords:
+      "information, internet, internet speed, broadband speed, download speed, upload speed",
+    robots: "index, follow",
+  },
+  {
+    path: "/history",
+    element: <TestHistory />,
+    title: "Test History | TIC Radar",
+    description: "Review all your past internet speed tests in one place.",
+    keywords: "test history, speed results, past tests",
+    robots: "index, follow",
+  },
+  {
+    path: "/my-isp",
+    element: <MyISP />,
+    title: "My ISP | TIC Radar",
+    description: "Review all your past internet speed tests in one place.",
+    keywords: "test history, speed results, past tests",
+    robots: "index, follow",
+  },
+  {
+    path: "/operator-performance",
+    element: <OperatorPerformance />,
+    title: "Operator Performance | TIC Radar",
+    description: "Review all your past internet speed tests in one place.",
+    keywords: "test history, speed results, past tests",
+    robots: "index, follow",
+  },
+  {
+    path: "/dashboard/:provinceName",
+    element: <Province />,
+    title: "Province Details | TIC Radar",
+    description: "Detailed insights into specific province metrics and data.",
+    keywords: "province, details, insights, metrics",
+    robots: "noindex, nofollow",
+  },
 ];
--- client-app/src/pages/speedtest/SpeedTest.jsx
+++ client-app/src/pages/speedtest/SpeedTest.jsx
@@ -11,17 +11,20 @@ import clock from "../../app/assets/image/clock.svg";
 import { styled } from "@mui/material/styles";
 import "./SpeedTest.css";
 import leftArrow from "../../app/assets/image/leftArrow.svg";
-import upload from "../../app/assets/image/uploadIcon.svg";
-import download from "../../app/assets/image/downloadIcon.svg";
+import uploadIcon from "../../app/assets/image/uploadIcon.svg";
+import downloadIcon from "../../app/assets/image/downloadIcon.svg";
 import clockIcon from "../../app/assets/image/clockIcon.svg";
 import DrawMeter from "./DrawMeter";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import moment from "moment-jalaali";
 import { convertToPersianNumbers } from "../../app/utils/convertToPersianNumbers";
 import elipse from "../../app/assets/image/elipse.svg";
 
 import { Link } from "react-router-dom";
 import ShowResult from "./ShowResult";
+import { STATUS_MAP } from "./constant";
+
+import io from "socket.io-client";
 
 const fadeIn = keyframes`
   from {
@@ -41,7 +44,6 @@ const SpeedTest = () => {
   });
   const [uploadSpeed, setUploadSpeed] = useState(null);
   const [testStage, setTestStage] = useState(null); // "ping", "download", "upload"
-  const [boxHeight, setBoxHeight] = useState("90dvh");
 
   const theme = useTheme();
   const isMD = useMediaQuery(theme.breakpoints.down("md"));
@@ -56,91 +58,112 @@ const SpeedTest = () => {
     setOpenDialog(false);
   };
 
-  const handleButtonClick = () => {
-    setIsGoButtonVisible(false);
-    setTimeout(() => {
-      setSpeedData((prev) => ({
-        ...prev,
-        ping: Math.floor(Math.random() * 31) + 50,
-      }));
-      setTestStage("init");
-    }, 1000);
-  };
-  const boxesData = [
-    { src: clockIcon, alt: "clockIcon", label: "پینگ:", value: speedData.ping },
-    {
-      src: download,
-      alt: "download",
-      label: "سرعت دانلود:",
-      value: speedData.downloadSpeed,
-    },
-    { src: upload, alt: "upload", label: "سرعت اپلود:", value: uploadSpeed },
-  ];
+  const [status, setStatus] = useState(2);
+  const [ping, setPing] = useState("0.0");
+  const [jitter, setJitter] = useState("0.0");
+  const [download, setDownload] = useState(0);
+  const [downloadProgress, setDownloadProgress] = useState(0);
+  const [upload, setUpload] = useState(0);
+  const [uploadProgress, setUploadProgress] = useState(0);
+  const [isDl, setIsDl] = useState(true);
+  const [clientIp, setClientIp] = useState("");
+
+  const [latency, setLatency] = useState(0);
+  const [socket, setSocket] = useState(null);
+  const [selectedServerURL, setSelectedServerURL] = useState(
+    "https://server1.eyesp.live"
+  );
 
   useEffect(() => {
-    if (testStage === "init") {
-      let index = 0;
-      let increment = true;  // A flag to determine whether to increment or decrement the index
-      const interval = setInterval(() => {
-        if (increment) {
-          index = index < 9 ? + index + 0.125 : index + 1;
-        } else {
-          index = index < 9 ? + index - 0.125 : index - 1;;
-        }
-        setSpeedData((prev) => ({
-          ...prev,
-          downloadSpeed: index,
-        }));
-        if (index >= 120) {
-          increment = false;  
-        } else if (index <= 0) {
-          increment = true; 
-        }
-      }, 1000 / 240);
-    } else if (testStage === "download") {
-      let timeElapsed = 0;
-      let initialDownload = parseFloat((Math.random() * 20 + 10).toFixed(2)); // random number between 10 and 30
+    fetch("https://server1.eyesp.live/get-ip")
+      .then((res) => res.json())
+      .then((data) => setClientIp(data.ip))
+      .catch((error) => console.error("Error fetching client IP:", error));
+  }, []);
 
-      const interval = setInterval(() => {
-        timeElapsed += 100;
-        if (timeElapsed <= 1000) {
-          setSpeedData((prev) => ({
-            ...prev,
-            downloadSpeed: initialDownload,
-          }));
-        } else if (timeElapsed > 1000 && timeElapsed <= 6000) {
-          let randomDifference = (Math.random() * 4 - 2).toFixed(2);
-          setSpeedData((prev) => ({
-            ...prev,
-            downloadSpeed: parseFloat(
-              (initialDownload + parseFloat(randomDifference)).toFixed(2)
-            ),
-          }));
-        } else {
-          clearInterval(interval);
-          setTimeout(() => {
-            setTestStage("upload");
-          }, 3000);
-        }
-      }, 100);
-      return () => clearInterval(interval);
-    } else if (testStage === "upload") {
-      let timeElapsed = 0;
-      let initialUpload = parseFloat((Math.random() * 10 + 5).toFixed(2));
+  const PING_TIMES = 10; // Number of pings
 
-      const interval = setInterval(() => {
-        timeElapsed += 100;
-        if (timeElapsed <= 500) {
-          setUploadSpeed(10 * (timeElapsed / 500));
-        } else if (timeElapsed > 500 && timeElapsed <= 5500) {
-          let randomDifference = (Math.random() * 4 - 2).toFixed(2);
-          setUploadSpeed(
-            parseFloat(
-              (initialUpload + parseFloat(randomDifference)).toFixed(2)
-            )
-          );
+  useEffect(() => {
+    if (!socket) {
+      const s = io(selectedServerURL);
+      setSocket(s);
+    } else {
+      socket.io.uri = selectedServerURL;
+      socket.connect();
+    }
+    return () => socket && socket.disconnect();
+  }, [selectedServerURL]);
+
+  useEffect(() => {
+    if (socket) {
+      let pingCount = 0;
+      let minLatency = Infinity;
+
+      socket.on("pong_event", async (timestamp) => {
+        const currentLatency = performance.now() - timestamp;
+        minLatency = Math.min(minLatency, currentLatency);
+        pingCount++;
+
+        if (pingCount === PING_TIMES) {
+          setLatency(minLatency.toFixed(2));
+          // await downloadTest();
         } else {
-          clearInterval(interval);
+          socket.emit("ping_event", performance.now());
+        }
+      });
+    }
+  }, [socket]);
+
+  const startPingTest = () => {
+    if (socket) {
+      socket.emit("ping_event", performance.now());
+    }
+  };
+
+  const mbpsToAmount = (s) => {
+    return 1 - 1 / Math.pow(1.3, Math.sqrt(s));
+  };
+
+  let flag = true;
+
+  const handleButtonClick = () => {
+    setIsGoButtonVisible(false);
+    startPingTest();
+    handleStart();
+  };
+
+  const handleStart = () => {
+    if (window.speedtest.getState() === STATUS_MAP.RUNNING) {
+      // 正在测速中
+    } else {
+      window.speedtest.onupdate = (data) => {
+        const {
+          dlProgress,
+          dlStatus,
+          ulProgress,
+          ulStatus,
+          pingStatus,
+          jitterStatus,
+        } = data;
+        setJitter(jitterStatus.length === 0 ? "0.0" : jitterStatus);
+        setPing(pingStatus.length === 0 ? "0.0" : pingStatus);
+        if (isDl && flag) {
+          setDownload(dlStatus);
+          console.log("dlProgress", dlProgress);
+          setDownloadProgress(dlStatus);
+          if (dlProgress == 1) {
+            setIsDl(false);
+            flag = false;
+            console.log("isDl", isDl);
+          }
+        }
+
+        if (!isDl || dlProgress == 1) {
+          setUpload(ulStatus);
+          setUploadProgress(ulProgress);
+        }
+
+        if (dlProgress == 1 && ulProgress == 1) {
           const currentJalaliDateInEnglish = moment().format("jYYYY/jM/jD");
           const currentJalaliDateInFarsi = convertToPersianNumbers(
             currentJalaliDateInEnglish
@@ -158,41 +181,154 @@ const SpeedTest = () => {
           const testResults = {
             date: currentJalaliDateInFarsi,
             time: convertToPersianNumbers(getCurrentTime()),
-            ping: convertToPersianNumbers(speedData.ping),
-            download: convertToPersianNumbers(speedData.downloadSpeed),
+            ping: convertToPersianNumbers(latency),
+            download: convertToPersianNumbers(dlStatus),
             testDuration: convertToPersianNumbers("00:16"),
             testType: "دقیق",
-            upload: convertToPersianNumbers(
-              parseFloat(
-                (
-                  initialUpload + parseFloat((Math.random() * 4 - 2).toFixed(2))
-                ).toFixed(2)
-              )
-            ),
+            upload: convertToPersianNumbers(ulStatus),
             server: "ایرانسل-تهران",
+            ip: clientIp,
           };
           const existingResults = JSON.parse(
             localStorage.getItem("testResults") || "[]"
           );
           existingResults.push(testResults);
           localStorage.setItem("testResults", JSON.stringify(existingResults));
+          setIsGoButtonVisible(true);
           setOpenDialog(true);
         }
-      }, 100);
-      return () => clearInterval(interval);
+      };
+      window.speedtest.onend = () => {
+        setStatus(STATUS_MAP.READY);
+      };
+      setStatus(STATUS_MAP.RUNNING);
+      window.speedtest.start();
     }
-  }, [testStage]);
+  };
 
-  useEffect(() => {
-    const navbarElement = document.querySelector(".nav-height");
+  // useEffect(() => {
+  //   if (testStage === "init") {
+  //     let index = 0;
+  //     let increment = true; // A flag to determine whether to increment or decrement the index
+  //     const interval = setInterval(() => {
+  //       if (increment) {
+  //         index =
+  //           index < 10 ? +index + 0.125 : index < 20 ? index + 0.5 : index + 1;
+  //       } else {
+  //         index =
+  //           index < 10 ? +index - 0.125 : index < 20 ? index - 0.5 : index - 1;
+  //       }
+  //       setSpeedData((prev) => ({
+  //         ...prev,
+  //         downloadSpeed: index,
+  //       }));
+  //       if (index >= 120) {
+  //         increment = false;
+  //       } else if (index <= 0) {
+  //         increment = true;
+  //       }
+  //     }, 1000 / 240);
+  //   } else if (testStage === "download") {
+  //     let timeElapsed = 0;
+  //     let initialDownload = parseFloat((Math.random() * 20 + 10).toFixed(2)); // random number between 10 and 30
 
-    if (navbarElement) {
-      const navbarHeight = navbarElement.offsetHeight;
-      setBoxHeight(`calc(98dvh - ${navbarHeight}px)`);
-    } else {
-      setBoxHeight("98dvh");
-    }
-  }, []);
+  //     const interval = setInterval(() => {
+  //       timeElapsed += 100;
+  //       if (timeElapsed <= 1000) {
+  //         setSpeedData((prev) => ({
+  //           ...prev,
+  //           downloadSpeed: initialDownload,
+  //         }));
+  //       } else if (timeElapsed > 1000 && timeElapsed <= 6000) {
+  //         let randomDifference = (Math.random() * 4 - 2).toFixed(2);
+  //         setSpeedData((prev) => ({
+  //           ...prev,
+  //           downloadSpeed: parseFloat(
+  //             (initialDownload + parseFloat(randomDifference)).toFixed(2)
+  //           ),
+  //         }));
+  //       } else {
+  //         clearInterval(interval);
+  //         setTimeout(() => {
+  //           setTestStage("upload");
+  //         }, 3000);
+  //       }
+  //     }, 100);
+  //     return () => clearInterval(interval);
+  //   } else if (testStage === "upload") {
+  //     let timeElapsed = 0;
+  //     let initialUpload = parseFloat((Math.random() * 10 + 5).toFixed(2));
+
+  //     const interval = setInterval(() => {
+  //       timeElapsed += 100;
+  //       if (timeElapsed <= 500) {
+  //         setUploadSpeed(10 * (timeElapsed / 500));
+  //       } else if (timeElapsed > 500 && timeElapsed <= 5500) {
+  //         let randomDifference = (Math.random() * 4 - 2).toFixed(2);
+  //         setUploadSpeed(
+  //           parseFloat(
+  //             (initialUpload + parseFloat(randomDifference)).toFixed(2)
+  //           )
+  //         );
+  //       } else {
+  //         clearInterval(interval);
+  //         const currentJalaliDateInEnglish = moment().format("jYYYY/jM/jD");
+  //         const currentJalaliDateInFarsi = convertToPersianNumbers(
+  //           currentJalaliDateInEnglish
+  //         );
+
+  //         const getCurrentTime = () => {
+  //           const now = new Date();
+  //           const hours = now.getHours().toString().padStart(2, "0");
+  //           const minutes = now.getMinutes().toString().padStart(2, "0");
+  //           const seconds = now.getSeconds().toString().padStart(2, "0");
+
+  //           return `${hours}:${minutes}:${seconds}`;
+  //         };
+
+  //         const testResults = {
+  //           date: currentJalaliDateInFarsi,
+  //           time: convertToPersianNumbers(getCurrentTime()),
+  //           ping: convertToPersianNumbers(speedData.ping),
+  //           download: convertToPersianNumbers(speedData.downloadSpeed),
+  //           testDuration: convertToPersianNumbers("00:16"),
+  //           testType: "دقیق",
+  //           upload: convertToPersianNumbers(
+  //             parseFloat(
+  //               (
+  //                 initialUpload + parseFloat((Math.random() * 4 - 2).toFixed(2))
+  //               ).toFixed(2)
+  //             )
+  //           ),
+  //           server: "ایرانسل-تهران",
+  //         };
+  //         const existingResults = JSON.parse(
+  //           localStorage.getItem("testResults") || "[]"
+  //         );
+  //         existingResults.push(testResults);
+  //         localStorage.setItem("testResults", JSON.stringify(existingResults));
+  //         setOpenDialog(true);
+  //       }
+  //     }, 100);
+  //     return () => clearInterval(interval);
+  //   }
+  // }, [testStage]);
+
+  const boxesData = [
+    { src: clockIcon, alt: "clockIcon", label: "پینگ:", value: latency },
+    {
+      src: downloadIcon,
+      alt: "download",
+      label: "سرعت دانلود:",
+      value: download,
+    },
+    {
+      src: uploadIcon,
+      alt: "upload",
+      label: "سرعت اپلود:",
+      value: upload,
+    },
+  ];
 
   const DesktopIP = () => {
     return (
@@ -382,7 +518,7 @@ const SpeedTest = () => {
                 }}
               >
                 <Typography variant="h3">ادرس IP:</Typography>
-                <Typography variant="h7">129.86.45.122</Typography>
+                <Typography variant="h7">{clientIp}</Typography>
               </Box>
               <Box
                 sx={{
@@ -447,23 +583,16 @@ const SpeedTest = () => {
                     }}
                   >
                     <DrawMeter
-                      amount={0.2}
                       bk={
                         /Trident.*rv:(\d+\.\d+)/i.test(navigator.userAgent)
                           ? "#45628A"
                           : "#1B70EE1C"
                       }
                       fg={"#1B70EE1C"}
-                      progress={0.3}
-                      prog={0.3}
-                      mbps={
-                        testStage === "init"
-                          ? speedData.downloadSpeed
-                          : testStage === "download"
-                          ? speedData.downloadSpeed
-                          : uploadSpeed
-                      }
-                      isDl={true}
+                      progress={isDl ? downloadProgress : uploadProgress}
+                      prog={isDl ? downloadProgress : uploadProgress}
+                      mbps={isDl ? download : upload}
+                      isDl={isDl}
                       theme="light"
                     />
                   </div>
@@ -501,9 +630,9 @@ const SpeedTest = () => {
       <ShowResult
         openDialog={openDialog}
         handleCloseDialog={handleCloseDialog}
-        ping={speedData.ping}
-        download={speedData.downloadSpeed}
-        upload={uploadSpeed}
+        ping={latency}
+        download={download}
+        upload={upload}
       />
     </>
   );
