2562c744800d7a0e861ec0359de30a25d416abc5
AliNikseresht <aliniksereshtdev@gmail.com>
2023-10-07 10:16:14 +0330

    fix: fix bug in speed test & containedselect & update palette

--- client-app/src/pages/speedtest/SpeedTest.jsx
+++ client-app/src/pages/speedtest/SpeedTest.jsx
@@ -38,6 +38,11 @@ import DrawMeterAnimate from "./DrawMeterAnimate";
 import axios from "axios";
 import {Link} from "react-router-dom";
 
+
+
+
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
+
 const fadeIn = keyframes`
   from { opacity: 0; }
   to { opacity: 1; }
