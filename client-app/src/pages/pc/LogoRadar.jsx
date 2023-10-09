14154024028ba011a3d8ec002c1f8110e56fb6b9
mahjs <mahjsdev@gamil.com>
2023-10-09 14:09:38 +0330

    fix: some ui issues

--- client-app/src/pages/pc/LogoRadar.jsx
+++ client-app/src/pages/pc/LogoRadar.jsx
@@ -9,13 +9,14 @@
 import React from "react";
 
 // Import Material-UI components and styles
-import { Box } from "@mui/material";
+import {Box} from "@mui/material";
 
 /**
  * Radar logo image asset.
  * @type {string}
  */
 import RadarLogo from "../../app/assets/image/Img-SpeedTest/Radar-logo.svg";
+import {useMediaQuery} from "@mui/material";
 
 /**
  * LogoRadar Component.
@@ -25,22 +26,17 @@ import RadarLogo from "../../app/assets/image/Img-SpeedTest/Radar-logo.svg";
  * @returns {JSX.Element} The RADAR GAME logo inside a styled container.
  */
 const LogoRadar = () => {
+  const isSmScreen = useMediaQuery((theme) => theme.breakpoints.down("sm"));
   return (
     <Box
       sx={{
-        width: "100%",
-        height: "9%",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "flex-start",
+        textAlign: isSmScreen ? "center" : "",
         px: "0.6rem",
-        userSelect: "none",
       }}
     >
       <img
         style={{
-          width: "auto",
-          height: "100%",
+          height: isSmScreen ? "3rem" : "4rem",
         }}
         src={RadarLogo}
         alt="Radar-Logo"
