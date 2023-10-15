30b20e4b089e2e5407bbdc033b75af43d149b2ff
mehrdadakbari <akbarmehrdad87@yahoo.com>
2023-10-15 14:58:21 +0330

    chart services

--- backend-app/app/Models/RstResult.php
+++ backend-app/app/Models/RstResult.php
@@ -52,38 +52,67 @@ class RstResult extends Model
         ]);
     }
 
-    public static function hourly($isp, $hours)
+    public static function hourly($isp = null, $hours)
     {
-        return self::where('isp', $isp)
-            ->where('date', now()->toDateString())
-            ->where('time', '>=', now()->subHours($hours)->toTimeString())->get();
+        $res = self::where('date', now()->toDateString())
+            ->where('time', '>=', now()->subHours($hours)->toTimeString());
+        if($isp) {
+            $res = $res->where('isp', $isp);
+        }
+        return $res->get();
     }
 
-    public static function daily($isp, $days)
+    public static function today($isp = null, $temp)
     {
-        return self::where('isp', $isp)
-            ->where('date', '>=', now()->subDays($days)->toDateString())
-            ->get();
+        $res = self::where('date', now()->toDateString());
+        if($isp) {
+            $res = $res->where('isp', $isp);
+        }
+        return $res->get();
     }
 
-    public static function weekly($isp, $weeks)
+    public static function daily($isp = null, $days)
     {
-        return self::where('isp', $isp)
-            ->where('date', '>=', now()->subWeeks($weeks)->toDateString())
-            ->get();
+        $res = self::where('date', '>=', now()->subDays($days)->toDateString());
+        if($isp) {
+            $res = $res->where('isp', $isp);
+        }
+        return $res->get();
     }
 
-    public static function monthly($isp, $months)
+    public static function weekly($isp = null, $weeks)
     {
-        return self::where('isp', $isp)
-            ->where('date', '>=', now()->subMonths($months)->toDateString())
-            ->get();
+        $res = self::where('date', '>=', now()->subWeeks($weeks)->toDateString());
+        if($isp) {
+            $res = $res->where('isp', $isp);
+        }
+        return $res->get();
     }
 
-    public static function yearly($isp, $years)
+    public static function monthly($isp = null, $months)
     {
-        return self::where('isp', $isp)
-            ->where('date', now()->subYears($years)->toDateString())
-            ->get();
+        $res = self::where('date', '>=', now()->subMonths($months)->toDateString());
+        if($isp) {
+            $res = $res->where('isp', $isp);
+        }
+        return $res->get();
+    }
+
+    public static function year($isp = null, $years)
+    {
+        $res = self::where('date', '>=', now()->format('Y').'-01-01');
+        if($isp) {
+            $res = $res->where('isp', $isp);
+        }
+        return $res->get();
+    }
+
+    public static function yearly($isp = null, $years)
+    {
+        $res = self::where('date', now()->subYears($years)->toDateString());
+        if($isp) {
+            $res = $res->where('isp', $isp);
+        }
+        return $res->get();
     }
 }
