79fec85ba1a2868d9d5fc2b7e5dd49b55a81faee
Sina Sepahvand <sina_sepahvand@email.kntu.ac.ir>
2023-10-16 12:48:45 +0400

    feat: add safe page

---
+++ client-app/src/app/common/CardInformation.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CardInformation = ({title, value, subTitle, subValue, color}) => {
+  return (
+    <Box>
+        
+    </Box>
+  )
+}
+
+export default CardInformation
