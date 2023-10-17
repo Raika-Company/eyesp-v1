79e1ce707034393922a379f75fc07503da43375f
AliNikseresht <aliniksereshtdev@gmail.com>
2023-10-17 16:00:54 +0330

    fix chart cunflict

--- client-app/src/app/common/Charts.jsx
+++ client-app/src/app/common/Charts.jsx
@@ -14,23 +14,17 @@ import {
   Area,
   Tooltip,
   ResponsiveContainer,
-  CartesianGrid,
+  ReferenceLine,
 } from "recharts";
 
 import YAxisLine from "./YAxisLine";
 import xAxisLight from "../../app/assets/image/time-compare-light.svg";
 import xAxisDark from "../../app/assets/image/time-compare-dark.svg";
-import { ContainedSelect } from "./ContainedSelect";
+import {ContainedSelect} from "./ContainedSelect";
+import {TodayCharts} from "../api/dashboard";
+import {useLocation} from "react-router-dom";
 
-/**
- * Custom tooltip component for the chart.
- *
- * @param {object} props - Component props.
- * @param {boolean} props.active - Indicates whether the tooltip is active.
- * @param {Array} props.payload - Data payload for the tooltip.
- * @returns {JSX.Element|null} - Tooltip component.
- */
-export const CustomTooltip = ({ active, payload }) => {
+export const CustomTooltip = ({active, payload}) => {
   if (active && payload && payload.length) {
     return (
       <div
@@ -57,12 +51,6 @@ export const CustomTooltip = ({ active, payload }) => {
 
   return null;
 };
-
-/**
- * Array containing chart titles and units.
- *
- * @type {Array}
- */
 const titlesChart = [
   {
     title: "سرعت دانلود",
@@ -81,49 +69,32 @@ const titlesChart = [
     unit: "Ms",
   },
 ];
-
-/**
- * Array containing chart colors.
- *
- * @type {Array}
- */
 const chartColors = [
   {stroke: "#008EDD", gradientStart: "#0091E3", gradientEnd: "#008EDD"},
   {stroke: "#FFD700", gradientStart: "#FFD740", gradientEnd: "#FFD700"},
   {stroke: "#FF0000", gradientStart: "#FF4040", gradientEnd: "#FF0000"},
   {stroke: "#008000", gradientStart: "#00A000", gradientEnd: "#008000"},
 ];
+export function GridItem({
+  theme,
+  rendered,
+  title,
+  data,
+  unit,
+  color,
+  background,
+}) {
+  const {pathname} = useLocation();
+  const [age, setAge] = useState("در حال حاضر");
+  const handleChangeDailyPercent = (event) => {
+    const selectedYear = event.target.value;
+    setAge(selectedYear);
 
-/**
- * Array containing form control items.
- *
- * @type {Array}
- */
-const FormControlItems = ["سالیانه", "سال 1402"];
-
-/**
- * Example data for the charts.
- *
- * @type {Array}
- */
-const data = [
-  ["سالیانه", "ماهیانه", "هفتگی"],
-  ["1402", "1401", "1400"],
-];
-
-/**
- * GridItem component to display a chart with data.
- *
- * @param {object} props - Component props.
- * @param {object} props.theme - MUI theme.
- * @param {boolean} props.rendered - Indicates whether the chart is rendered.
- * @param {string} props.title - Chart title.
- * @param {string} props.unit - Unit for chart data.
- * @param {Array} props.data - Chart data.
- * @param {object} props.color - Chart color.
- * @returns {JSX.Element} - GridItem component.
- */
-function GridItem({ theme, rendered, title, data, unit, color }) {
+    // For the sake of debugging, directly set percentages based on options
+    if (selectedYear === "در حال حاضر") setPercentage(65);
+    else if (selectedYear === "1 روز قبل") setPercentage(75);
+    else if (selectedYear === "1 هفته قبل") setPercentage(85);
+  };
   return (
     <NewCardContainer
       sx={{
@@ -138,72 +109,114 @@ function GridItem({ theme, rendered, title, data, unit, color }) {
       }}
     >
       <Box display="flex" position="relative" width="92%">
-        <Box sx={{ width: "100%" }}>
-          <Box
-            display="flex"
-            flexWrap="wrap"
-            justifyContent="space-between"
-            alignItems="flex-end"
-          >
-            <Typography color="text.main" variant="h4" gutterBottom>
+        <Box sx={{width: "100%"}}>
+          <Box display="flex" justifyContent="space-between">
+            <Typography
+              color="text.main"
+              variant="h1"
+              component="h2"
+              gutterBottom
+            >
               {title}
             </Typography>
-            <Box
-              borderRadius="3rem"
-              paddingRight="3%"
-              width="100%"
-              height="250px"
-            >
-              {rendered && (
-                <Box>
-                  <ResponsiveContainer width="100%" height={265}>
-                    <AreaChart width="100%" height="100%" data={data}>
-                      <Tooltip content={<CustomTooltip />} />
-                      <CartesianGrid
-                        vertical={false}
-                        stroke={
-                          theme.palette.mode === "dark" ? "#2e2e2e" : "#E9E9E9"
-                        }
-                      />
-                      <defs>
-                        <filter
-                          id="glow"
-                          x="-70%"
-                          y="-70%"
-                          width="200%"
-                          height="200%"
-                        >
-                          <feOffset
-                            result="offOut"
-                            in="SourceGraphic"
-                            dx="0"
-                            dy="0"
-                          />
-                          <feGaussianBlur
-                            result="blurOut"
-                            in="offOut"
-                            stdDeviation="5"
-                          />
-                          <feBlend
-                            in="SourceGraphic"
-                            in2="blurOut"
-                            mode="normal"
-                          />
-                        </filter>
-                      </defs>
-                      <Area
-                        type="linear"
-                        dataKey="value"
-                        stroke={color.stroke}
-                        fill={`url(#gradientChart${color.stroke})`}
-                        strokeWidth={4}
-                        filter="url(#glow)"
-                      />
-                    </AreaChart>
-                  </ResponsiveContainer>
-                </Box>
-              )}
-            </Box>
+            {title === "سرعت دانلود" && pathname === "/my-isp" && (
+              <FormControl sx={{width: "25%", marginLeft: "3rem"}}>
+                <ContainedSelect
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={age}
+                  label="سال"
+                  onChange={handleChangeDailyPercent}
+                  displayEmpty
+                >
+                  <MenuItem value="در حال حاضر">درحال حاضر</MenuItem>
+                  <MenuItem value="هفتگی">هفتگی</MenuItem>
+                  <MenuItem value="ماهانه">ماهانه</MenuItem>
+                  <MenuItem value="سالانه">سالانه</MenuItem>
+                </ContainedSelect>
+              </FormControl>
+            )}
+          </Box>
+          <Box
+            borderRadius="3rem"
+            paddingRight="3%"
+            // sx={{
+            //   background:
+            //     theme.palette.mode === "dark"
+            //       ? "radial-gradient(646.45% 156.82% at 1.67% -6.71%, rgba(103, 154, 202, 0.23) 0.31%, rgba(104, 137, 151, 0.00) 100%)"
+            //       : "radial-gradient(646.45% 156.82% at 1.67% -6.71%, #E2F7FF 0.31%, rgba(188, 203, 209, 0.00) 100%)",
+            // }}
+            width="100%"
+            height="250px"
+          >
+            {rendered && (
+              <Box>
+                <ResponsiveContainer width="100%" height={220}>
+                  <AreaChart width="100%" height="100%" data={data}>
+                    <Tooltip content={<CustomTooltip />} />
+                    <ReferenceLine y={5} stroke="grey" strokeWidth={1} />
+                    <ReferenceLine y={25} stroke="grey" strokeWidth={1} />
+                    <ReferenceLine y={40} stroke="grey" strokeWidth={1} />
+                    <ReferenceLine y={60} stroke="grey" strokeWidth={1} />
+                    <ReferenceLine y={78} stroke="grey" strokeWidth={1} />
+                    <ReferenceLine y={95} stroke="grey" strokeWidth={1} />
+
+                    <defs>
+                      {/* <linearGradient
+                        id={`gradientChart${color.stroke}`}
+                        x1="0"
+                        y1="0"
+                        x2="0"
+                        y2="1"
+                      >
+                        <stop
+                          offset="0.333333"
+                          stopColor={color.gradientStart}
+                          stopOpacity="0.167089"
+                        />
+                        <stop
+                          offset="1"
+                          stopColor={color.gradientEnd}
+                          stopOpacity="0"
+                        />
+                      </linearGradient> */}
+                      <filter
+                        id="glow"
+                        x="-70%"
+                        y="-70%"
+                        width="200%"
+                        height="200%"
+                      >
+                        <feOffset
+                          result="offOut"
+                          in="SourceGraphic"
+                          dx="0"
+                          dy="0"
+                        />
+                        <feGaussianBlur
+                          result="blurOut"
+                          in="offOut"
+                          stdDeviation="5"
+                        />
+                        <feBlend
+                          in="SourceGraphic"
+                          in2="blurOut"
+                          mode="normal"
+                        />
+                      </filter>
+                    </defs>
+                    <Area
+                      type="linear"
+                      dataKey="value"
+                      stroke={color && color.stroke}
+                      fill={`url(#gradientChart${color && color.stroke})`}
+                      strokeWidth={4}
+                      filter="url(#glow)"
+                    />
+                  </AreaChart>
+                </ResponsiveContainer>
+              </Box>
+            )}
           </Box>
           <img
             src={theme.palette.mode === "light" ? xAxisLight : xAxisDark}
@@ -212,7 +225,7 @@ function GridItem({ theme, rendered, title, data, unit, color }) {
           />
         </Box>
         <YAxisLine
-          max={Math.max(...data.map((line) => line.value))}
+          // max={Math.max(...data.map((line) => line.value))}
           unit={unit}
         />
       </Box>
@@ -220,92 +233,36 @@ function GridItem({ theme, rendered, title, data, unit, color }) {
   );
 }
 
-/**
- * Generates random data for the chart.
- *
- * @returns {Array} - Random chart data.
- */
 function generateRandomData() {
+  // Generate random data for the chart
   const data = [];
   for (let i = 1; i <= 12; i++) {
     data.push({
       month: `${i} ماه`,
-      value: Math.floor(Math.random() * 100),
+      value: Math.floor(Math.random() * 100), // Adjust the range as needed
     });
   }
   return data;
 }
-
-/**
- * Charts component to display multiple charts.
- *
- * This component renders multiple charts, allowing the user to select different data sets and view them in graphical form.
- *
- * @returns {JSX.Element} - Charts component.
- * @component
- */
 const Charts = () => {
   const theme = useTheme();
-  const [ispData, setIspData] = useState([]);
   const [rendered, setRendered] = useState(false);
-  const [formControlItems, setFormControlItems] = useState("");
-  const [currentChartData, setCurrentChartData] = useState({});
-  const [randomChartData1, setRandomChartData1] = useState([]);
-  const [randomChartData2, setRandomChartData2] = useState([]);
-  const [randomChartData3, setRandomChartData3] = useState([]);
-  const [randomChartData4, setRandomChartData4] = useState([]);
-  const isSmScreen = useMediaQuery((theme) => theme.breakpoints.down("sm"));
+  const [currentChartData, setCurrentChartData] = useState(generateRandomData);
   const isMdScreen = useMediaQuery((theme) => theme.breakpoints.down("md"));
-  const isLgScreen = useMediaQuery((theme) => theme.breakpoints.down("lg"));
 
-  /**
-   * Event handler for form control item selection.
-   *
-   * @param {Object} event - The event object.
-   */
-  const handleChange = (event) => {
-    setFormControlItems(event.target.value);
-    const selectedISPData = ispData.find(
-      (item) => item.id === event.target.value
-    );
-    if (selectedISPData) {
-      setCurrentChartData(selectedISPData);
-      setRandomChartData1(generateRandomData());
-      setRandomChartData2(generateRandomData());
-      setRandomChartData3(generateRandomData());
-      setRandomChartData4(generateRandomData());
-    }
-  };
-
-  /**
-   * Fetches chart data from a JSON source and sets the initial chart data.
-   */
   useEffect(() => {
-    axios
-      .get("/data/chartData.json")
+    TodayCharts()
       .then((response) => {
-        const data = response.data;
-        setIspData(data);
-        const defaultISPData = data.find((item) => item.id === "ایرانسل"); // find "ایرانسل" data
-        if (defaultISPData) {
-          setCurrentChartData(defaultISPData); // set "ایرانسل" data as default chart data
-          setRandomChartData1(generateRandomData());
-          setRandomChartData2(generateRandomData());
-          setRandomChartData3(generateRandomData());
-          setRandomChartData4(generateRandomData());
-        }
+        console.log(response);
       })
       .catch((error) => {
         console.log("خطا در بارگذاری اطلاعات", error);
       });
   }, []);
-
-  /**
-   * Marks the charts as rendered once during component initialization.
-   */
   useEffect(() => {
     setRendered(true);
   }, []);
+
   return (
     <>
       <NewCardContainer
@@ -314,55 +271,9 @@ const Charts = () => {
           overflowY: "scroll",
           marginTop: "1rem",
           flexBasis: isMdScreen ? "100%" : "50%",
-          pl: "1rem",
         }}
       >
-        <Box
-          sx={{
-            display: "flex",
-            flexWrap: "wrap",
-            alignItems: "center",
-            justifyContent: "space-evenly",
-          }}
-        >
-          <Box
-            sx={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}
-          >
-            {FormControlItems.map((items, index) => (
-              <FormControl
-                key={index}
-                sx={{
-                  m: "0.4rem",
-                  width: isSmScreen ? 125 : isMdScreen ? 180 : 160,
-                  borderRadius: "25px",
-                }}
-                size="small"
-              >
-                <ContainedSelect
-                  labelId={`demo-select-small-label-${index}`}
-                  id={`demo-select-small-${index}`}
-                  label={items}
-                  onChange={handleChange}
-                  displayEmpty
-                >
-                  <MenuItem disabled>
-                    <span style={{ color: "#676767" }}>{items}</span>
-                  </MenuItem>
-                  {data[index].map((menuItem, menuItemIndex) => (
-                    <MenuItem
-                      sx={{ color: "text.main" }}
-                      key={menuItemIndex}
-                      value={menuItem}
-                    >
-                      {menuItem}
-                    </MenuItem>
-                  ))}
-                </ContainedSelect>
-              </FormControl>
-            ))}
-          </Box>
-        </Box>
-        <Grid>
+        <Grid container gap={2.5}>
           {titlesChart.map((line, index) => (
             <GridItem
               key={index}
@@ -370,20 +281,11 @@ const Charts = () => {
               rendered={rendered}
               title={line.title}
               unit={line.unit}
-              data={
-                // Assign each chart a different randomChartData
-                index === 0
-                  ? randomChartData1
-                  : index === 1
-                  ? randomChartData2
-                  : index === 2
-                  ? randomChartData3
-                  : randomChartData4
-              }
-              color={chartColors[index]} // Pass the color prop here
+              color={chartColors[index]}
+              data={currentChartData}
             />
           ))}
-        </Grid>
+        </Grid>{" "}
       </NewCardContainer>
     </>
   );
